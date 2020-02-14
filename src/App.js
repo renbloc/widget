2dcbcfcc0b8374bdb018daa44b804d2318030dea
renbloc <oscar@renbloc.com>
2020-02-14 12:15:37 +0100

    changed a stupid thing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world, we are renbloc lock up your daughters cause we are here to.... 
+          RENBLOC RULES
         </p>
         <a
           className="App-link"
