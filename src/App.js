77284760e40a7eaed50fb90861d1c7a50e1f6746
renbloc <oscar@renbloc.com>
2020-02-14 12:11:36 +0100

    small change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world, we are renbloc lock up your daughters cause we are here to.... 
         </p>
         <a
           className="App-link"
